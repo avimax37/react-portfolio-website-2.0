06456bb451d8a422500989796151978a259024e2
Avinaba Bera <avimax37@gmail.com>
2022-08-05 14:47:33 +0530

    Update Services and services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './services.css'
-import { BiCheck } from 'react-icons/bi'
+import { FiCheck } from 'react-icons/fi'
 
 const Services = () => {
   return (
@@ -17,28 +17,28 @@ const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              <FiCheck className='service__list-icon' />
+              <p>Working closely with developers to convert designs into a working product.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              <FiCheck className='service__list-icon' />
+              <p>Making high-fidelity wireframes and layouts to show what the final design will look like.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              <FiCheck className='service__list-icon' />
+              <p>Seeking to make apps and websites visually appealing and easy to navigate.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              <FiCheck className='service__list-icon' />
+              <p>Performing user testing to validate design decisions and identify problems.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              <FiCheck className='service__list-icon' />
+              <p>Conducting user research to identify any goals, needs, behaviors, and pain points involved with a product interaction.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+              <FiCheck className='service__list-icon' />
+              <p>Developing user personas based on target customers.</p>
             </li>
           </ul>
         </article>
@@ -52,31 +52,31 @@ const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
           </ul>
@@ -91,27 +91,27 @@ const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
             <li>
-              <BiCheck className='service__list-icon' />
+              <FiCheck className='service__list-icon' />
               <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
             </li>
           </ul>
